4a23266cffda4563be0c94f7172cf11e12e52063
NicolasLeon05 <132603045+NicolasLeon05@users.noreply.github.com>
2026-06-11 20:45:09 -0300

    Add destroy octree children function

--- Assets/Scripts/OctreeNode.cs
+++ Assets/Scripts/OctreeNode.cs
@@ -1,16 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
-using static Unity.VisualScripting.Metadata;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class OctreeNode : MonoBehaviour
 {
     [SerializeField] private float size;
     private List<OctreeNode> children = new List<OctreeNode>();
     private const int CHILDREN_AMOUNT = 8;
+    private OctreeNode parent = null;
+
 
     public List<OctreeNode> Children => children;
     public float Size => size;
+    public AABB Bounds => new AABB(transform.position, new Vector3(size, size, size));
+    public OctreeNode Parent => parent;
 
     private static readonly Vector3[] Directions = new Vector3[]
     {
@@ -32,6 +34,7 @@ public class OctreeNode : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.green;
         Gizmos.DrawWireCube(transform.position, new Vector3(size, size, size));
     }
 
@@ -52,9 +55,29 @@ public class OctreeNode : MonoBehaviour
             OctreeNode childNode = childGO.AddComponent<OctreeNode>();
             Vector3 childPosition = transform.position + (Directions[i] * delta);
             childNode.Initialize(childPosition, newSize);
-
+            childNode.SetParent(this);
             children.Add(childNode);
         }
     }
 
+    [ContextMenu("Destroy Children")]
+    private void DestroyChildren()
+    {
+        Debug.Log("Destroy called, children amount: " + children.Count);
+        if (children.Count == 0)
+            return;
+
+        foreach (var child in children)
+        {
+            child.DestroyChildren();
+            DestroyImmediate(child.gameObject);
+        }
+
+        children.Clear();
+    }
+
+    public void SetParent(OctreeNode parent)
+    {
+        this.parent = parent;
+    }
 }
